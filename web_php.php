8f5d575f121afa0049a27dc5be8cc6b6721bf1eb
NatVazh <130495351+NatVazh@users.noreply.github.com>
2024-03-24 14:58:56 +0300

    Update web_php.php

--- web_php.php
+++ web_php.php
@@ -1,1 +1,9 @@
-<? echo("hello world");?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Тестируем PHP</title>
+    </head>
+    <body>
+        <?php echo '<p>Привет, мир!</p>'; ?>
+    </body>
+</html>
